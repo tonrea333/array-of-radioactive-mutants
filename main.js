e438a77163cab73562fa8bceab26688f57ed5fb0
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-30 09:27:51 -0400

    all

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd(name) {//adds "Colin" to the end of a given array
+  name.push("Colin")
+  console.log(name)
+}
+function addMeToStart(name) {//adds "Colin" to the start of a given array
+  name.unshift("Colin")
+}
+function changeLast(number, newNumber) {//changes the last element in the given array to the given value
+  number.pop()
+  number.push(newNumber);
+  console.log(number)
+  return number
+}
 
+function changeAllValuesTo(number, newValue) {//changes every value in the given array to the given value
+  console.log(number);
+  console.log(newValue);
+  while (number[0] !== newValue){//Note if newValue is in array this may not work
+    if (number[0] !== newValue) {
+      number.shift();
+      number.push(newValue);
+      console.log(number)
+    }
+  }
+}
+function oddOrEven(number){
+  console.log(number);
 
+let numberEven = number % 2
+let numberOdd = number
+numberDivisable = "odd"
+console.log(numberDivisable)
+} 
+
+function changeNextThreeToValue(initialValue, numberList, changeValue){
+  console.log(initialValue)
+  console.log(numberList)
+  console.log(changeValue)
+}
 
 
 
---
+++ test.js
@@ -0,0 +1,6 @@
+function gridMap(fn,a) {
+    return [[]];
+    console.log(fn)
+    console.log(a)
+    
+  }
